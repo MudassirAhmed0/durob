84d80558eb6b29c58d09ace92d65a87f7231dd4a
MudassirAhmed0 <mudassir0ahmed1@gmail.com>
2024-08-24 18:26:34 +0500

    privacy-terms-integrated

--- src/components/common/PrivacyTermsContainer.js
+++ src/components/common/PrivacyTermsContainer.js
@@ -1,3 +1,4 @@
+import { checkIfExist } from "@/utils/checkIfExist";
 import React from "react";
 
 const PrivacyTermsContainer = ({ privacyTerms }) => {
@@ -8,7 +9,7 @@ const PrivacyTermsContainer = ({ privacyTerms }) => {
         className="container1680 py180 pt-[unset] lg:mt-[4.16666666667vw] sm:mt-[50px] mt-[40px]"
       >
         <div
-          dangerouslySetInnerHTML={{ __html: privacyTerms }}
+          dangerouslySetInnerHTML={{ __html: checkIfExist(privacyTerms) }}
           className="lg:w-[59.1666666667vw] w-full PrivacyTerms"
         ></div>
       </div>
